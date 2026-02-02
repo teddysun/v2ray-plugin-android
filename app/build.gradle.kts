1e373b62e39f56d4241f22054768220be0d305aa
Teddysun <i@teddysun.com>
2026-02-02 22:52:00 +0900

    Bump version 5.44.1
    Signed-off-by: Teddysun <i@teddysun.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 35
-        versionCode = 5043000
-        versionName = "5.43.0"
+        versionCode = 5044010
+        versionName = "5.44.1"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
